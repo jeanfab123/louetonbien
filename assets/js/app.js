c2f4e4949e5c940ce3e25188bae0aafd6f807378
FABULAS Jean-Pierre <jfabulas@gmail.com>
2019-07-08 12:15:42 +0200

    Modify app.js, comoser.json

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,9 @@
 // require('../css/app.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js')
+const $ = require('jquery')
+
+$(document).on('click', '.mega-dropdown', function (e) {
+  e.stopPropagation()
+})
